935e8c05adba613e6150373391a3f1a97539acb2
Franzinhusgamepleiers <112588490+Franzinhusgamepleiers@users.noreply.github.com>
2023-09-25 20:22:08 -0300

    Add files via upload

---
+++ screens/StarMap.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class StarMapScreen extends Component {
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+                <Text>Tela do mapa estelar!</Text>
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+    },
+    routeCard: {
+        flex: 0.12,
+        justifyContent: "center",
+        alignItems: "center",
+        margin: 10,
+        marginLeft: 30,
+        marginRight: 30,
+        borderRadius: 100,
+        backgroundColor: "white"
+}})
